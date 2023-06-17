eadb827b0ccb8af80b689a069e8a4a5719eebe45
YungNewton <isaacnewtonahanmisi@gmail.com>
2023-06-17 22:04:55 +0100

    commit

--- app.js
+++ app.js
@@ -1,47 +1,27 @@
 const express = require('express');
 const body_parser = require('body-parser');
+const { todo } = require('node:test');
 const app = express();
 const port = 8080;
 app.use(body_parser.urlencoded({extended: true}))
 
 app.set('view engine', 'ejs');
 
+var todos =["hey"];
 var today = new Date();
-var day = today.getDay();
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-var yyyy = today.getFullYear();
-today = dd + '/' + mm + '/' + yyyy;
-
-
+var options = {
+    weekDay: "long",
+    day: "numeric",
+    month: "long"
+}
+var day = today.toLocaleDateString("en-US", options);
 app.get('/', (req, res) => {
-    switch(day){
-        case 0:
-            res.render('index', {days: "Sunday", month: mm, year: yyyy});
-            break;
-        case 1:
-            res.render('index', {days: "Monday", month: mm, year: yyyy});
-            break;
-        case 2:
-            res.render('index', {days: "Tuesday", month: mm, year: yyyy});
-            break;
-        case 3:
-            res.render('index', {days: "Wednesday", month: mm, year: yyyy});
-            break;
-        case 4:
-            res.render('index', {days: "Thursday", month: mm, year: yyyy});
-            break;
-        case 5:
-            res.render('index', {days: "Friday", month: mm, year: yyyy});
-            break;
-        case 6:
-            res.render('index', {days: "Saturday", month: mm, year: yyyy});
-            break;
-    }
+    res.render('index', {days: day, ItemsList: todos});
+    res.send("")
 });
 
-app.get('/',(req, res)=>{
-    res.send(today);
+app.post('/',(req, res)=>{
+    res.redirect('/');
 })
 
 app.listen(port,()=>{
